bdd872e9468ba17b021c6a1769f57234e1c45b53
John Herson <john.d.herson@gmail.com>
2015-10-19 09:38:35 -0400

    Update LogEventConsumer.java

--- src/main/java/com/nowellpoint/aws/lambda/LogEventConsumer.java
+++ src/main/java/com/nowellpoint/aws/lambda/LogEventConsumer.java
@@ -23,6 +23,12 @@ import com.amazonaws.services.logs.model.OutputLogEvent;
  * AWS Lambda Function to retrieve log events from AWS CloudWatch and send to Loggly using the REST endpoint
  * - LOGGLY_API_KEY included in source for demo purposeses but shoud be handled in a secure way
  * - Lambda function is setup to execute every 5 minutes 
+ * - Steps
+ * - 1. Describe all Log Groups in CloudWatch
+ * - 2. Iterate over log groups and describe Log Streams
+ * - 3. Iterate over log streams getting Log Events
+ * - 4. Build LogEntry string
+ * - 5. Execute HTTP GET to Loggly REST API
  * */
 
 public class LogEventConsumer {
